e0474f7e0cc6b8224ac327ec63e9348a94e6757b
Luciano Viola <86131882+LushoGV@users.noreply.github.com>
2023-05-26 00:09:09 -0300

    Update index.ts

--- src/pages/api/convert/index.ts
+++ src/pages/api/convert/index.ts
@@ -12,7 +12,8 @@ export default async function Handler(
   try {
     if(id == null || id.length == 0) return res.status(200).json({message: "Debe ingresar una URL"});
 
-    const videoId = id.toString().substring(id.indexOf("v=")+2, id.indexOf("&"))
+    const videoId = id.toString().startsWith("https://youtu.be/") ? id.toString().substring(id.lastIndexOf("/")+1) : id.toString().substring(id.indexOf("v=")+2, id.indexOf("&"))
+    
     const response = await axios.get(
       `https://youtube-mp36.p.rapidapi.com/dl?id=${videoId}`,
       {
